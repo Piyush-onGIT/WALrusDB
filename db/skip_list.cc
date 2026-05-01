bbbc1fe9883cec10a8a73bd5732771c5a4e86b45
Piyush <thisissahupiyush@gmail.com>
2026-05-01 18:14:44 +0530

    Added search function, optimized insertion

--- db/skip_list.cc
+++ db/skip_list.cc
@@ -1,5 +1,4 @@
 #include "skip_list.h"
-#include <iostream>
 
 SkipList::SkipList() {
   head_ = new SkipListNode();
@@ -13,9 +12,10 @@ SkipList::SkipList() {
 int SkipList::LevelPromoter() {
   int level = 0;
 
-  while (level < MAX_LEVEL) {
+  while (level < MAX_LEVEL - 1) {
     int coin = rand() % 2;
-    if (coin == 0) break;
+    if (coin == 0)
+      break;
     level++;
   }
 
@@ -25,43 +25,51 @@ int SkipList::LevelPromoter() {
 void SkipList::Insert(std::string key, std::string val) {
   int level = LevelPromoter();
   current_top_level_ = std::max(current_top_level_, level);
-  current_top_level_ = std::min(current_top_level_, static_cast<int>(MAX_LEVEL));
-  int level_itr_ = 0;
+  int level_itr = current_top_level_;
 
-  while (level_itr_ <= level) {
-    SkipListNode* itr_node = head_->next_[level_itr_];
-    SkipListNode* update_node = head_;
-    SkipListNode* new_node = new SkipListNode();
-    new_node->key_ = key;
-    new_node->val_ = val;
+  SkipListNode *node_itr = head_;
+  SkipListNode *new_node = new SkipListNode();
+  new_node->key_ = key;
+  new_node->val_ = val;
 
-    if (itr_node == nullptr) {
-      head_->next_[level_itr_] = new_node;
-      level_itr_++;
-      continue;
+  for (level_itr; level_itr >= 0; level_itr--) {
+    while (node_itr->next_[level_itr] != nullptr &&
+           node_itr->next_[level_itr]->key_ < key) {
+      node_itr = node_itr->next_[level_itr];
     }
-
-    while (itr_node && itr_node->key_ < key) {
-      update_node = itr_node;
-      itr_node = itr_node->next_[level_itr_];
-    }
-
-    SkipListNode* tmp = update_node->next_[level_itr_];
-    update_node->next_[level_itr_] = new_node;
-    new_node->next_[level_itr_] = tmp;
-
-    level_itr_++;
+    if (level_itr > level) continue;
+    
+    SkipListNode* tmp = node_itr->next_[level_itr];
+    node_itr->next_[level_itr] = new_node;
+    new_node->next_[level_itr] = tmp;
   }
 }
 
-void SkipList::Delete(std::string key) {
+std::optional<std::string> SkipList::Search(std::string key) {
+  if (head_ == nullptr)
+    return std::nullopt;
 
+  int level_itr = current_top_level_;
+  SkipListNode *node_itr = head_;
+
+  for (level_itr; level_itr >= 0; level_itr--) {
+    while (node_itr->next_[level_itr] != nullptr &&
+           node_itr->next_[level_itr]->key_ < key) {
+      node_itr = node_itr->next_[level_itr];
+    }
+  }
+
+  if (node_itr->next_[0] != nullptr && node_itr->next_[0]->key_ == key)
+    return node_itr->next_[0]->val_;
+  return std::nullopt;
 }
+
+void SkipList::Delete(std::string key) {}
 
 void SkipList::PrintSkipList() {
   for (int i = current_top_level_; i >= 0; i--) {
     std::cout << "Level " << i << ": ";
-    SkipListNode* itr = head_->next_[i];
+    SkipListNode *itr = head_->next_[i];
     while (itr) {
       std::cout << "(" << itr->key_ << ":" << itr->val_ << ")->";
       itr = itr->next_[i];
